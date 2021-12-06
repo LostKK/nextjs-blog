302c20a552d348b58e4bfa075ae1ac2024810a04
LostKK <1395175536@qq.com>
2021-12-06 18:18:26 +0800

    music-list

--- pages/component/music/index.js
+++ pages/component/music/index.js
@@ -12,12 +12,16 @@ export default function Music() {
 
     return (
         <Slide direction="right" in={checked} mountOnEnter unmountOnExit timeout={1500}>
-           <div className={classnames(styles.music_wrap)}>
-             <div className={classnames(styles.title)}>
-                I Miss <br/> My Bar
-             </div>
+            <div className={classnames(styles.music_wrap)}>
+                <div className={classnames(styles.title)}>
+                    I Miss <br /> My Bar
+                </div>
+                <div>
+                    <iframe frameBorder={"no"} border={"0"} marginWidth={"0"} marginHeight="0" width={330} height={86} src={"//music.163.com/outchain/player?type=2&id=1474959445&auto=1&height=66"}>
+                    </iframe>
+                </div>
             </div>
         </Slide>
-        
+
     )
 }
--- pages/component/playlist/index.js
+++ pages/component/playlist/index.js
@@ -1,42 +1,89 @@
 import styles from './index.module.scss'
 import classnames from 'classnames'
 import Slide from '@mui/material/Slide';
+import Slider from '@mui/material/Slider';
 import React, { useState, useEffect } from 'react';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import _ from 'lodash'
 
 export default function Playlist() {
+    var audioArray = []
     const [checked, setChecked] = useState(false);
-
-    const list = [
+    var [list, setList] = useState([
         {
-            name:'rain',
-            music: '../../../public/assets/music/rain.mp3'
-        }
-    ]
+            name: 'rain',
+            music: '../../../assets/music/rain.mp3',
+            active: false
+        },
+        {
+            name: 'thunder',
+            music: '../../../assets/music/thunder.mp3',
+            active: false
+        },
+        {
+            name: 'loudThunder',
+            music: '../../../assets/music/loudThunder.mp3',
+            active: false
+        },
+    ])
 
     useEffect(() => {
         setChecked(true);
     });
 
+    const changeList = index => {
+        let temp = _.cloneDeep(list)
+        temp[index].active = !temp[index].active
+        setList(temp)
+    }
+
     return (
         <Slide direction="left" in={checked} mountOnEnter unmountOnExit timeout={1500}>
-           <div className={classnames(styles.list_wrap)}>
-               <div className={classnames(styles.list_header)}>
+            <div className={classnames(styles.list_wrap)}>
+                <div className={classnames(styles.list_header)}>
                     <div className={classnames(styles.header_single)}>share</div>
                     <div className={classnames(styles.header_single)}>about</div>
-               </div>
-              <div>
-                 {list.map((i,index) =>{
-                     return (
-                         <div key={index}>
-                             {i.name}
-                            <audio loop={"loop"} preload={"auto"} src={i.music} controls={true}>
-                            </audio>
-                        </div>
-                     )
-                 })}
-              </div>
+                </div>
+                <div className={classnames(styles.list_detail)}>
+                    {list.map((i, index) => {
+                        return (
+                            <div key={index}>
+                                <div className={classnames(styles.detail_title)}>{i.name}</div>
+                                <div style={{ display: 'flex' }}>
+                                    {
+                                        (!list[index].active && <PlayArrowIcon
+                                            onClick={() => {
+                                                changeList(index)
+                                                audioArray[index].play()
+                                            }}
+                                        />)
+                                    }
+                                    {
+                                        (list[index].active && <PauseIcon
+                                            onClick={() => {
+                                                changeList(index)
+                                                audioArray[index].pause()
+                                            }}
+                                        />)
+                                    }
+                                    <Slider
+                                        defaultValue={1}
+                                        max={1}
+                                        min={0}
+                                        step={0.1}
+                                        onChange={e => audioArray[index].volume = e.target.value}
+                                    style={{ width: '80%' }}
+                                    />
+                                </div>
+                                <audio src={i.music} loop ref={node => audioArray[index] = node}>
+                                </audio>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </Slide>
-        
+
     )
 }
